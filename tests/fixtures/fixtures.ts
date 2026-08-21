96da82e08bdb945184f6da80661a41cb29908060
Zhuk_Dmitry <fgh298@rambler.ru>
2026-08-21 19:16:44 +0300

    added mainPage fixture

---
+++ tests/fixtures/fixtures.ts
@@ -0,0 +1,19 @@
+import { test as base } from '@playwright/test'
+import { MainPage } from '../pages/MainPage'
+
+type MyFixtures = {
+  mainPage: MainPage
+}
+
+export const test = base.extend<MyFixtures>({
+  mainPage: async ({ page }, use) => {
+    const mainPage = new MainPage(page)
+    await mainPage.open()
+    await mainPage.closeModalSubscribe()
+    await mainPage.closeNotificationPopup()
+    await mainPage.closeCookiesPopup()
+
+    await use(mainPage)
+  },
+})
+export { expect } from '@playwright/test'
--- tests/spects/unauthorized/mainPage.spec.ts
+++ tests/spects/unauthorized/mainPage.spec.ts
@@ -1,34 +1,13 @@
-import test from '@playwright/test'
-import { MainPage } from '../../pages/MainPage'
+import { test, expect } from '../../fixtures'
 
-test('Открытие главное страницы', async ({ page }) => {
-  const mainPage = new MainPage(page)
-  await mainPage.open()
-})
-
-test('Проверка доступности элементов хедера', async ({ page }) => {
-  const mainPage = new MainPage(page)
-  await mainPage.open()
-  await mainPage.closeModalSubscribe()
-  await mainPage.closeNotificationPopup()
-  await mainPage.closeCookiesPopup()
+test('Проверка доступности элементов хедера', async ({ mainPage }) => {
   await mainPage.headerHasCorrectAriaSnapshot()
 })
 
-test('Проверка доступности элементов табов категорий', async ({ page }) => {
-  const mainPage = new MainPage(page)
-  await mainPage.open()
-  await mainPage.closeModalSubscribe()
-  await mainPage.closeNotificationPopup()
-  await mainPage.closeCookiesPopup()
+test('Проверка доступности элементов табов категорий', async ({ mainPage }) => {
   await mainPage.categoriesTabsHasCorrectAriaSnapshot()
 })
 
-test('Проверка доступности элементов бокового меню', async ({ page }) => {
-  const mainPage = new MainPage(page)
-  await mainPage.open()
-  await mainPage.closeModalSubscribe()
-  await mainPage.closeNotificationPopup()
-  await mainPage.closeCookiesPopup()
+test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.sidebarMenuHasCorrectAriaSnapshot()
 })
