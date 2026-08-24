24ce4c82a711e95050d8bba8e4e09b59cd1b5ec1
Zhuk_Dmitry <fgh298@rambler.ru>
2026-08-24 16:06:12 +0300

    Snapshot tests added for AddDropdownMenu, NotificationPopup, and DisabledAdsModal

--- tests/pages/MainPage.ts
+++ tests/pages/MainPage.ts
@@ -5,26 +5,91 @@ export class MainPage extends BasePage {
   private readonly headerLocator: Locator
   private readonly categoriesTabsLocator: Locator
   private readonly sidebarMenuLocator: Locator
+  private readonly headerAddBtnLocator: Locator
+  private readonly headerNotificationBtnLocator: Locator
+  private readonly headerSafeModeBtnLocator: Locator
+  private readonly headerDisableAdsBtnLocator: Locator
+  private readonly headerLoginBtnLocator: Locator
+  private readonly headerAddDropdownMenuLocator: Locator
+  private readonly headerNotificationPopupLocator: Locator
+  private readonly headerDisabledAdsModalLocator: Locator
 
   constructor(page: Page) {
     super(page)
 
+    // Хедер
     this.headerLocator = this.page.getByRole('banner')
+    this.headerAddBtnLocator = this.headerLocator.getByRole('button', { name: 'Добавить' })
+    this.headerNotificationBtnLocator = this.headerLocator.getByRole('button', { name: 'Уведомления' })
+    this.headerSafeModeBtnLocator = this.headerLocator.getByRole('button', { name: 'перейти в безопасный режим' })
+    this.headerDisableAdsBtnLocator = this.headerLocator.getByRole('button', { name: 'Отключить рекламу' })
+    this.headerLoginBtnLocator = this.headerLocator.getByRole('button', { name: 'Вход' })
+    this.headerAddDropdownMenuLocator = this.page.getByRole('menu').filter({
+      has: this.page.getByText('Загрузить видео или Shorts'),
+    })
+    this.headerNotificationPopupLocator = this.page
+      .getByRole('dialog')
+      .locator('.wdp-notifications-popup-module__wrapper')
+    this.headerDisabledAdsModalLocator = this.page.getByRole('dialog', { name: 'RUTUBE' })
+    // Табы категорий
     this.categoriesTabsLocator = this.page.getByTestId('homepage-navigation')
+    // Боковое меню
     this.sidebarMenuLocator = this.page.getByLabel('Облегченная панель навигации')
   }
 
+  // Actions
   async open() {
     await this.page.goto('https://rutube.ru/')
   }
+  async openHeaderAddDropdownMenu() {
+    await this.headerAddBtnLocator.click()
+  }
+  async openHeaderNotificationPopup() {
+    await this.headerNotificationBtnLocator.click()
+  }
+  async activatedSafeMode() {
+    await this.headerSafeModeBtnLocator.click()
+  }
+  async openHeaderDisabledAdsModal() {
+    await this.headerDisableAdsBtnLocator.click()
+  }
+  async openLoginPage() {
+    await this.headerLoginBtnLocator.click()
+  }
 
+  // Snapshots
   async headerHasCorrectAriaSnapshot() {
-    await expect(this.headerLocator).toMatchAriaSnapshot()
+    await expect(this.headerLocator).toMatchAriaSnapshot({ name: 'headerAriaSnapshot.yml' })
   }
   async categoriesTabsHasCorrectAriaSnapshot() {
-    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot()
+    await expect(this.categoriesTabsLocator).toMatchAriaSnapshot({ name: 'categoriesTabsAriaSnapshot.yml' })
   }
   async sidebarMenuHasCorrectAriaSnapshot() {
-    await expect(this.sidebarMenuLocator).toMatchAriaSnapshot()
+    await expect(this.sidebarMenuLocator).toMatchAriaSnapshot({ name: 'sidebarMenuAriaSnapshot.yml' })
+  }
+  async headerAddDropdownMenuContainerHasCorrectAriaSnapshot() {
+    await expect(this.headerAddDropdownMenuLocator).toMatchAriaSnapshot({
+      name: 'headerAddDropdownMenuContainerAriaSnapshot.yml',
+    })
+  }
+  async headerNotificationPopupHasCorrectAriaSnapshot() {
+    await expect(this.headerNotificationPopupLocator).toMatchAriaSnapshot({
+      name: 'headerNotificationPopupAriaSnapshot.yml',
+    })
+  }
+  async headerDisabledAdsModalHasCorrectAriaSnapshot() {
+    await expect(this.headerDisabledAdsModalLocator).toMatchAriaSnapshot({
+      name: 'headerDisabledAdsModalAriaSnapshot.yml',
+    })
+  }
+
+  // other checks
+  async headerAddDropdownMenuHasCorrectItems() {
+    await expect(
+      this.headerAddDropdownMenuLocator.locator('button').filter({ hasText: 'Загрузить видео или Shorts' }),
+    ).toBeVisible()
+    await expect(
+      this.headerAddDropdownMenuLocator.locator('button').filter({ hasText: 'Создать трансляцию' }),
+    ).toBeVisible()
   }
 }
--- tests/spects/unauthorized/mainPage.spec.ts
+++ tests/spects/unauthorized/mainPage.spec.ts
@@ -11,3 +11,20 @@ test('Проверка доступности элементов табов ка
 test('Проверка доступности элементов бокового меню', async ({ mainPage }) => {
   await mainPage.sidebarMenuHasCorrectAriaSnapshot()
 })
+
+test('Проверка доступности элементов выпадающего меню добавления контента', async ({ mainPage }) => {
+  await mainPage.openHeaderAddDropdownMenu()
+  await mainPage.headerAddDropdownMenuContainerHasCorrectAriaSnapshot()
+  await mainPage.headerAddDropdownMenuHasCorrectItems()
+})
+
+test('Проверка доступности элементов попапа уведомлений', async ({ mainPage }) => {
+  await mainPage.openHeaderNotificationPopup()
+  await mainPage.headerNotificationPopupHasCorrectAriaSnapshot()
+})
+
+test('Проверка доступности элементов модального окна отключения рекламы', async ({ mainPage }) => {
+  await mainPage.openHeaderDisabledAdsModal()
+  await mainPage.headerDisabledAdsModalHasCorrectAriaSnapshot()
+  await expect(mainPage.page).toHaveURL('https://rutube.ru/?popup=premium_product_ad_flow&productCode=RUTUBE')
+})
